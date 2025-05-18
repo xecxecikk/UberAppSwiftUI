9b8c5f4e211e21f027cad763d0810985c45f13cd
xecxecikk <xecekrdnz_94@outlook.com>
2025-05-18 19:57:45 +0300

    Initial Commit

---
+++ UberAppSwiftUI/UberAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberAppSwiftUIApp.swift
+//  UberAppSwiftUI
+//
+//  Created by XECE on 18.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct UberAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
